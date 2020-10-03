c95e9b2546b07a0037922f1b27e6630133bd83b5
Ravi reddy <reddyraviteja74@gmail.com>
2020-10-03 12:14:27 +0530

    farenheit to celcius

---
+++ cf.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class cf{
+public static void main (String args[])
+{
+   int c;
+   int f;
+   
+   Scanner sc=new Scanner(System.in);
+   f=sc.nextInt();
+   c= (f-32)*5/9;
+   System.out.println(c);
+}
+}
+   
